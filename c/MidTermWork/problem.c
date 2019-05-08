b1ab3f7878b859110825336fc965d463c682530f
864872429 <46430061+864872429@users.noreply.github.com>
2019-05-08 20:40:46 +0800

    Update problem.c

--- c/MidTermWork/problem.c
+++ c/MidTermWork/problem.c
@@ -1,81 +1,157 @@
-#include "Problem.h"
+/****************************************************
 
-void GetProblem(int Problem[10][4])
+Copyright (c) <2019> <copyright holders:lumanke>
+
+"Anti 996" License Version 1.0
+https://github.com/996icu/996.ICU/blob/master/LICENSE"
+
+@Version:4.0(c)
+@Author:lumanke
+@LastEditors: lumanke
+@Description:Midterm work
+@Date: 2019-03-01 20:44:11
+@LastEditTime: 2019-05-08 19:08:11
+4
+****************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define RANDOM(x) (rand() % x)
+
+struct Problem *GetProblem(struct Problem *);
+void GetGrade(struct Problem *);
+void File(struct Problem *, int);
+void End(struct Problem *);
+
+struct Problem
 {
-	int i = 0;
-	while (i < 10)
-	{
-		Problem[i][0] = random(50);
-		Problem[i][1] = random(2);
-		Problem[i][2] = random(50);
-		switch (Problem[i][1])
-		{
-		case 0:
-			Problem[i][1] = '+';
-			if (Problem[i][0] + Problem[i][2] > 50)
-			{
-				continue;
-			}
-			Problem[i][3] = Problem[i][0] + Problem[i][2];
-			break;
-		case 1:
-			Problem[i][1] = '-';
-			if (Problem[i][0] - Problem[i][2] < 0)
-			{
-				continue;
-			}
-			Problem[i][3] = Problem[i][0] - Problem[i][2];
-		}
-		i++;
-	}
+    int problem[10][2];
+    int grade;
+    FILE *fp;
+};
+
+int main()
+{
+    struct Problem student;
+    srand((int)time(NULL));
+    while (1)
+    {
+        student.grade = 0;
+        system("cls");
+        File(&student, 1);
+        GetGrade(GetProblem(&student));
+        End(&student);
+    }
 }
 
-void GetGrade(int Problem[10][4], int* grade)
+struct Problem *GetProblem(struct Problem *p)
 {
-	int i, j, answer;
-	for (i = 0; i < 10; i++)
-	{
-		printf("\n-----------------------------------------------------------------------------\nProblem %d :   \n", i + 1);
-		for (j = 0; j < 3; j++)
-		{
-			printf("%d%c%d=", Problem[i][0], Problem[i][1], Problem[i][2]);
-			scanf("%d", &answer);
-			if (answer == Problem[i][3])
-			{
-				WrongOrCorrect(0);
-				*grade += (20 + j * j - 7 * j) / 2;
-				break;
-			}
-			else if (j == 2)
-				printf("%d%c%d=%d\n", Problem[i][0], Problem[i][1], Problem[i][2], Problem[i][3]);
-			WrongOrCorrect(1);
-		}
-	}
+    int i = 0;
+    while (i < 10)
+    {
+        p->problem[i][0] = rand() % 50 + 1;
+        p->problem[i][1] = rand() % 101 - 50;
+        if ((p->problem[i][0] + p->problem[i][1] > 50) || (p->problem[i][0] + p->problem[i][1] < 0))
+            continue;
+        i++;
+    }
+    return p;
 }
 
+void GetGrade(struct Problem *p)
+{
+    int i, j, answer;
+    void WrongOrCorrect(int);
+    void NotebookOfWrong(int a, int b, struct Problem *p);
+    for (i = 0; i < 10; i++)
+    {
+        printf("\n-----------------------------------------------------------------------------\nYou grade is %d now\nproblem %d :   \n", p->grade, i + 1);
+        for (j = 0; j < 3; j++)
+        {
+            fflush(stdin);
+            printf("%d%+d=", p->problem[i][0], p->problem[i][1]);
+            scanf("%d", &answer);
+            if (answer == p->problem[i][0] + p->problem[i][1])
+            {
+                WrongOrCorrect(0);
+                p->grade += (20 + j * j - 7 * j) / 2;
+                break;
+            }
+            else if (j == 2)
+            {
+                if (p->fp)
+                    NotebookOfWrong(p->problem[i][0], p->problem[i][1], p);
+                printf("正确答案为:\n%d%+d=%d\n", p->problem[i][0], p->problem[i][1], p->problem[i][0] + p->problem[i][1]);
+            }
+            WrongOrCorrect(1);
+        }
+    }
+}
+
+void End(struct Problem *p)
+{
+    char i;
+    printf("-----------------------------------------------------------------------------\n-----------------------------------------------------------------------------\nyou grade is %d\n", p->grade);
+    switch (p->grade / 10)
+    {
+    case 10:
+    case 9:
+        printf("SMART\n");
+        break;
+    case 8:
+        printf("GOOD\n");
+        break;
+    case 7:
+        printf("OK\n");
+        break;
+    case 6:
+        printf("PASS\n");
+        break;
+    default:
+        printf("TRY AGAIN\n");
+    }
+    printf("would you like try again(y/n)\n");
+    fflush(stdin);
+    i = getchar();
+    if (i == 'N' | 'n')
+    {
+        File(p, 0);
+        exit(0);
+    }
+}
 
 void WrongOrCorrect(int i)
 {
-	char put[8][40] = { "Very good!","xcellent!","Nice work!","Keep up the good work!","Wrong.Try once more.","No.Don\'t give up!","Not correct.Keep trying.","No.Please try again." };
-	puts(*(put + random(4) + i * 4));
+    char *put[8] = {"Very good!", "xcellent!", "Nice work!", "Keep up the good work!", "Wrong.Try once more.",
+                    "No.Don't give up!", "Not correct.Keep trying.", "No.Please try again."};
+    puts(*(put + RANDOM(4) + i * 4));
 }
 
-void End(int* grade)
+void File(struct Problem *p, int x)
 {
-	printf("-----------------------------------------------------------------------------\n-----------------------------------------------------------------------------\nyou grade is %d\n", *grade);
-	switch (*grade / 10)
-	{
-	case 10:
-	case 9: printf("SMART\n"); break;
-	case 8: printf("GOOD\n"); break;
-	case 7: printf("OK\n"); break;
-	case 6: printf("PASS\n"); break;
-	default: printf("TRY AGAIN\n");
-	}
-	*grade = 0;
-	printf("would you like try again(y/n)\n");
-	if (getchar() == 'N' || getchar() == 'n')
-	{
-		exit(0);
-	}
+    if (x)
+    {
+        p->fp = fopen("错题本.txt", "a");
+        if (p->fp)
+        {
+            printf("错题本功能已开启(存储在同目录下\"错题本.txt\"\n");
+            fprintf(p->fp, "下面是你的错题\n");
+        }
+        else
+        {
+            printf("无法打开错题本\n,改功能自动关闭\n");
+        }
+    }
+    else
+    {
+        fclose(p->fp);
+    }
+}
+
+void NotebookOfWrong(int a, int b, struct Problem *p)
+{
+    printf("该题已加入错题本\n");
+    fprintf(p->fp, "%d%+d=%d\n", a, b, a + b);
 }
