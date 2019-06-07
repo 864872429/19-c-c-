bf9f820ccbd0fe2dd227711ace02971291aad488
dingyang666 <46430061+dingyang666@users.noreply.github.com>
2019-06-08 02:50:20 +0800

    更新

--- cpp/Fileoperation.cpp
+++ cpp/Fileoperation.cpp
@@ -2,66 +2,219 @@
 
 Fileoperation::Fileoperation()
 {
-	teacher_file.open("teacher.txt", ios::in);
-	if (!teacher_file.is_open())
-	{
-		cout << "teacher.txt can not open\n";
-		exit(1);
-	}
-	while (!teacher_file.eof())
-	{
-		Teacher temp = {};
-		teacher_file >> temp.tea_num >> temp.name >> temp.password >> temp.phone >> temp.n;
-		teacher_data.push_back(temp);
-	}
-	student_file.open("student.txt", ios::in);
-	if (!(student_file.is_open()))
-	{
-		cout << "student.txt can not open\n";
-		exit(1);
-	}
-	while (!student_file.eof())
-	{
-		Student temp = {};
-		student_file >> temp.stu_num >> temp.name >> temp.password >> temp.CLASS >> temp.phone >> temp.topic_size;
-		if (temp.topic_size)
-			temp.topic = new int[temp.topic_size];
-		else
-			temp.topic = NULL;
-		for (int i = 0; i < temp.topic_size; i++)
-			student_file >> temp.topic[i];
-		student_file >> temp.n;
-		student_data.push_back(temp);
-	}
+	fileRead(15);
 }
 
-vector<Student> Fileoperation::getstudent_data() const
+vector<User> Fileoperation::get_student_data()
 {
 	return student_data;
 }
 
-vector<Teacher> Fileoperation::getteacher_data() const
+vector<Admin> Fileoperation::get_teacher_data()
 {
 	return teacher_data;
 }
 
-void Fileoperation::setstudent_data(vector<Student>temp)
+vector<Report> Fileoperation::get_report_data()
 {
-	student_data = temp;
+	return report_data;
 }
 
-void Fileoperation::setteacher(vector<Teacher>temp)
+vector<Problem> Fileoperation::get_problem_data()
+{
+	return problem_data;
+}
+
+void Fileoperation::set_student_data(vector<User>temp)
+{
+	student_data = temp;
+	fileWrite(1);
+}
+
+void Fileoperation::set_teacher_data(vector<Admin>temp)
 {
 	teacher_data = temp;
+	fileWrite(2);
+}
+
+void Fileoperation::set_report_data(vector<Report> temp)
+{
+	report_data = temp;
+	fileWrite(4);
+}
+
+void Fileoperation::set_problem_data(vector<Problem>temp)
+{
+	problem_data = temp;
+	fileWrite(8);
+}
+
+void Fileoperation::fileWrite(int x)
+{
+	if (x & 1)
+	{
+		student_file.open("student.txt", ios::out | ios::trunc);
+		if (!(student_file.is_open()))
+		{
+			cout << "student.txt can not open\n";
+			exit(1);
+		}
+		for (int i = 0; i < student_data.size(); i++)
+		{
+			if (i < student_data.size() - 1)
+				student_file << student_data[i].stu_num << " " << student_data[i].name << " " << student_data[i].password << " " << student_data[i].CLASS << " " << student_data[i].phone << " " << student_data[i].topic << " " << student_data[i].n << endl;
+			else
+				student_file << student_data[i].stu_num << " " << student_data[i].name << " " << student_data[i].password << " " << student_data[i].CLASS << " " << student_data[i].phone << " " << student_data[i].topic << " " << student_data[i].n;
+		}
+		student_file.close();
+	}
+	if (x & 2)
+	{
+		teacher_file.open("teacher.txt", ios::out | ios::trunc);
+		if (!teacher_file.is_open())
+		{
+			cout << "teacher.txt can not open\n";
+			exit(1);
+		}
+		for (int i = 0; i < teacher_data.size(); i++)
+		{
+			if (i < teacher_data.size() - 1)
+				teacher_file << teacher_data[i].tea_num << " " << teacher_data[i].name << " " << teacher_data[i].password << " " << teacher_data[i].phone << " " << teacher_data[i].n << endl;
+			else
+				teacher_file << teacher_data[i].tea_num << " " << teacher_data[i].name << " " << teacher_data[i].password << " " << teacher_data[i].phone << " " << teacher_data[i].n;
+		}
+		teacher_file.close();
+	}
+	if (x & 4)
+	{
+		report_file.open("report.txt", ios::out | ios::trunc);
+		if (!report_file.is_open())
+		{
+			cout << "report.txt can not open\n";
+			exit(1);
+		}
+		for (int i = 0; i < report_data.size(); i++)
+		{
+			if (i < report_data.size() - 1)
+				report_file << report_data[i].problem_mun << endl << report_data[i].stu_num << endl << report_data[i].status << endl << report_data[i].content << endl;
+			else
+				report_file << report_data[i].problem_mun << endl << report_data[i].stu_num << endl << report_data[i].status << endl << report_data[i].content;
+		}
+		report_file.close();
+	}
+	if (x & 8)
+	{
+		problem_file.open("problem.txt", ios::out | ios::trunc);
+		if (!problem_file.is_open())
+		{
+			cout << "problem.txt can not open\n";
+			exit(1);
+		}
+		for (int i = 0; i < problem_data.size(); i++)
+		{
+			if (i < problem_data.size() - 1)
+			{
+				problem_file << problem_data[i].tea_num << endl << problem_data[i].munber << endl << problem_data[i].instruction << endl << problem_data[i].max_num << endl << problem_data[i].current_num << endl << problem_data[i].status << endl;
+				if (problem_data[i].status == "-1")
+					problem_file << problem_data[i].delete_reason << endl;
+			}
+			else
+			{
+				if (problem_data[i].status != "-1")			
+					problem_file << problem_data[i].tea_num << endl << problem_data[i].munber << endl << problem_data[i].instruction << endl << problem_data[i].max_num << endl << problem_data[i].current_num << endl << problem_data[i].status;			
+				else
+					problem_file << problem_data[i].tea_num << endl << problem_data[i].munber << endl << problem_data[i].instruction << endl << problem_data[i].max_num << endl << problem_data[i].current_num << endl << problem_data[i].status << problem_data[i].delete_reason;
+			}
+		}
+		problem_file.close();
+	}
+}
+
+
+void Fileoperation::fileRead(int x)
+{
+	if (x & 1)
+	{
+		student_data.clear();
+		student_file.open("student.txt", ios::in);
+		if (!(student_file.is_open()))
+		{
+			cout << "student.txt can not open\n";
+			exit(1);
+		}
+		while (!student_file.eof())
+		{
+			User temp = {};
+			student_file >> temp.stu_num >> temp.name >> temp.password >> temp.CLASS >> temp.phone >> temp.topic >> temp.n;
+			student_data.push_back(temp);
+		}
+		student_file.close();
+	}
+	if (x & 2)
+	{
+		teacher_data.clear();
+		teacher_file.open("teacher.txt", ios::in);
+		if (!teacher_file.is_open())
+		{
+			cout << "teacher.txt can not open\n";
+			exit(1);
+		}
+		while (!teacher_file.eof())
+		{
+			Admin temp = {};
+			teacher_file >> temp.tea_num >> temp.name >> temp.password >> temp.phone >> temp.n;
+			teacher_data.push_back(temp);
+		}
+		teacher_file.close();
+	}
+	if (x & 4)
+	{
+		report_data.clear();
+		report_file.open("report.txt", ios::in);
+		if (!report_file.is_open())
+		{
+			cout << "report.txt can not open\n";
+			exit(1);
+		}
+		while (!report_file.eof())
+		{
+			Report temp = {};
+			getline(report_file, temp.problem_mun);
+			getline(report_file, temp.stu_num);
+			getline(report_file, temp.status);
+			if (temp.status != "-1")
+				getline(report_file, temp.content);
+			report_data.push_back(temp);
+		}
+		report_file.close();
+	}
+	if (x & 8)
+	{
+		problem_data.clear();
+		problem_file.open("problem.txt", ios::in);
+		if (!problem_file.is_open())
+		{
+			cout << "problem.txt can not open\n";
+			exit(1);
+		}
+		while (!problem_file.eof())
+		{
+			Problem temp = {};
+			getline(problem_file, temp.tea_num);
+			getline(problem_file, temp.munber);
+			getline(problem_file, temp.instruction);
+			getline(problem_file, temp.max_num);
+			getline(problem_file, temp.current_num);
+			getline(problem_file, temp.status);
+			if(temp.status =="-1")
+				getline(problem_file, temp.delete_reason);
+			problem_data.push_back(temp);		
+		}
+		problem_file.close();
+	}
 }
 
 Fileoperation::~Fileoperation()
 {
-	student_file.close();
-	teacher_file.close();
-	for (int i = 0; i < student_data.size(); i++)
-	{
-		delete []student_data[i].topic;
-		student_data[i].topic = NULL;
-	}
+	fileWrite(15);
 }
--- cpp/Fileoperation.h
+++ cpp/Fileoperation.h
@@ -8,48 +8,78 @@ using namespace std;
 #ifndef _FILEOPERATION_H
 #define _FILEOPERATION_H
 
-struct Student
+struct User                  //因为Student被作为类名了，就叫User吧。
 {
-	string stu_num;  //学号
-	string name;     //姓名
-	string password; //密码
-	string CLASS;    //班级，class是关键字，所以大写
-	string phone;    //电话
-	int* topic;      //选题题号数组
-	int topic_size;  //选题数
-	bool n;          //若被删除，该值为0
+	string stu_num;          //学号
+	string name;             //姓名
+	string password;         //密码
+	string CLASS;            //班级，class是关键字，所以大写
+	string phone;            //电话
+	string topic;               //选题号 -1表示未选题 0表示待审核
+	bool n;                  //若被删除，该值为0
 	//其他数据待补充
 };
 
-struct Teacher
+struct Admin                 //因为Teacher被作为类名了，就叫Admin吧。
 {
-	string tea_num;  //工号
-	string name;     //姓名
-	string password; //密码
-	string phone;    //电话
-	bool n;          //若被删除，该值为0
+	string tea_num;          //工号
+	string name;             //姓名
+	string password;         //密码
+	string phone;            //电话
+	bool n;                  //若被删除，该值为0
 	//其他数据待补充
 };
 
+struct Report                //选题报告
+{
+	string problem_mun;      //题号
+	string stu_num;          //选题学生学号
+	string status;           //状态(-1-未提交 0-待审核 1-已通过 2-未通过 3-被老师删除)
+	string content;          //报告内容
+	//其他数据待补充
+};
+
+struct Problem                    //题目
+{
+	string tea_num;               //发布老师工号
+	string munber;                //题号
+	string instruction;           //说明
+	string max_num;               //最大选题人数
+	string current_num;           //当前选题人数
+	string status;                //若被删除，该值为0，若为-1，则有删除理由
+	string delete_reason;         //删除理由
+	//其他数据待补充
+};
 
 class Fileoperation
 {
 public:
 	Fileoperation();
 
-	vector< Student > getstudent_data() const;
-	vector< Teacher > getteacher_data() const;
+	vector<User> get_student_data();
+	vector<Admin> get_teacher_data();
+	vector<Report> get_report_data();
+	vector<Problem> get_problem_data();
 
-	void setstudent_data(vector< Student >temp);
-	void setteacher(vector< Teacher >temp);
+	void set_student_data(vector<User>temp);
+	void set_teacher_data(vector<Admin>temp);
+	void set_report_data(vector<Report>temp);
+	void set_problem_data(vector<Problem>temp);
+
+
+	void fileWrite(int);//输入0-15;1-学生，2-老师，4-报告，8-选题。
+	void fileRead(int); //输入0-15;1-学生，2-老师，4-报告，8-选题。
 
 	~Fileoperation();
-
 private:
 	fstream student_file;
 	fstream teacher_file;
-	vector< Student > student_data;
-	vector< Teacher > teacher_data;
+	fstream report_file;
+	fstream problem_file;
+	vector<User> student_data;
+	vector<Admin> teacher_data;
+	vector<Report> report_data;
+	vector<Problem> problem_data;
 };
 
 #endif // !Fileoperation.h
